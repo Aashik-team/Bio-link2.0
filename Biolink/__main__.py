6f2d6329d3e90273ddb890f6711396eca58998ff
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-12-03 22:38:18 +0530

    #𝐀𝐚𝐬𝐡𝐢𝐤 𝐓ᴇᴀᴍ

--- Biolink/__main__.py
+++ Biolink/__main__.py
@@ -1,16 +1,16 @@
 import asyncio
 import importlib
 from pyrogram import idle
-from MAFU import MAFU
-from MAFU.modules import ALL_MODULES
+from Biolink import Biolink
+from Biolink.modules import ALL_MODULES
 from config import LOGGER_ID, BOT_USERNAME
 
 loop = asyncio.get_event_loop()
 
 async def roy_bot():
     for all_module in ALL_MODULES:
-        importlib.import_module("MAFU.modules." + all_module)
-    print("• @AviaBeatzBot B𝗈𝗍 Started Successfully.")
+        importlib.import_module("Biolink.modules." + all_module)
+    print("• @aashikmusicbot B𝗈𝗍 Started Successfully.")
     await idle()
     print("• Don't edit baby, otherwise you get an error: @networkxlog")
     await MAFU.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @shivang_xd**")
