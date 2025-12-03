55710a69f48bdcc156871cbdbf255ea1c88cc376
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-12-03 22:33:50 +0530

    #𝐀𝐚𝐬𝐡𝐢𝐤 𝐓ᴇᴀᴍ

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+from os import getenv
+
+# ------------------------------------------------
+
+API_ID = int(os.environ.get("API_ID", "28795512"))
+API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
+
+# ------------------------------------------------
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7629767021:AAEIOuI9VzAMsGkmMVH5RfpM2PblKDeBoZ0")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "AviaBeatzBot")
+
+# -----------------------------------------------
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "8056154987"))
+SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8056154987"))
+# ------------------------------------------------
+
+# ------------------------------------------------
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002465641501"))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-1002275259183"))
+
+# ------------------------------------------------
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://shivang:shiv@cluster0.2bupd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# ------------------------------------------------
